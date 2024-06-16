dc381bc9cfa071258b8eefc50d3daf8730592348
MichalGeron <128380877+MichalGeron@users.noreply.github.com>
2024-06-16 21:16:05 +0300

    Updating pages to client app

--- client/src/components/CodeBlock.js
+++ client/src/components/CodeBlock.js
@@ -3,56 +3,62 @@ import { useParams } from 'react-router-dom';
 import io from 'socket.io-client';
 import hljs from 'highlight.js';
 import 'highlight.js/styles/default.css';
+import axios from 'axios';
 
 const socket = io('http://localhost:4000');
 
 const CodeBlock = () => {
-  const { id } = useParams();
-  const [codeBlock, setCodeBlock] = useState({ title: '', code: '' });
-  const [role, setRole] = useState('');
+    const { id } = useParams();
+    const [code, setCode] = useState('');
+    const [isMentor, setIsMentor] = useState(false);
+    const [solution, setSolution] = useState('');
 
-  useEffect(() => {
-    const userRole = prompt('Enter your role (mentor/student):');
-    setRole(userRole);
-    socket.emit('join', { codeBlockId: id, role: userRole });
-    
-    socket.on('loadCodeBlock', (block) => {
-      setCodeBlock(block);
-    });
+    useEffect(() => {
+        axios.get(`http://localhost:4000/codeblocks/${id}`)
+            .then(response => {
+                setCode(response.data.code);
+                setIsMentor(response.data.isMentor);
+                setSolution(response.data.solution);
+            })
+            .catch(error => console.error(error));
+    }, [id]);
 
-    socket.on('codeUpdate', (newCode) => {
-      setCodeBlock((prevBlock) => ({ ...prevBlock, code: newCode }));
-    });
+    useEffect(() => {
+        socket.emit('join', id);
+        socket.on('codeUpdate', newCode => {
+            setCode(newCode);
+        });
+        return () => socket.off('codeUpdate');
+    }, [id]);
 
-    return () => {
-      socket.disconnect();
+    const handleCodeChange = (e) => {
+        setCode(e.target.value);
+        socket.emit('codeChange', { room: id, code: e.target.value });
     };
-  }, [id]);
 
-  const handleCodeChange = (event) => {
-    const newCode = event.target.value;
-    setCodeBlock((prevBlock) => ({ ...prevBlock, code: newCode }));
-    socket.emit('codeChange', { codeBlockId: id, code: newCode });
-  };
+    useEffect(() => {
+        hljs.highlightAll();
+    }, [code]);
 
-  useEffect(() => {
-    hljs.highlightAll();
-  }, [codeBlock.code]);
+    useEffect(() => {
+        if (code === solution) {
+            alert('😃');
+        }
+    }, [code, solution]);
 
-  return (
-    <div>
-      <h1>{codeBlock.title}</h1>
-      {role === 'mentor' ? (
-        <pre>
-          <code className="javascript">
-            {codeBlock.code}
-          </code>
-        </pre>
-      ) : (
-        <textarea value={codeBlock.code} onChange={handleCodeChange}></textarea>
-      )}
-    </div>
-  );
+    return (
+        <div>
+            <h1>Code Block</h1>
+            {isMentor ? (
+                <pre>
+                    <code className="javascript">{code}</code>
+                </pre>
+            ) : (
+                <textarea value={code} onChange={handleCodeChange} />
+            )}
+            {code === solution && <div style={{ fontSize: '2em' }}>😃</div>}
+        </div>
+    );
 };
 
 export default CodeBlock;
--- client/src/components/Lobby.js
+++ client/src/components/Lobby.js
@@ -1,20 +1,18 @@
-// src/components/Lobby.js
-
 import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Lobby = () => {
-  return (
-    <div>
-      <h1>Choose code block</h1>
-      <ul>
-        <li><Link to="/codeblock/1">Async case</Link></li>
-        <li><Link to="/codeblock/2">Callback hell</Link></li>
-        <li><Link to="/codeblock/3">Promise example</Link></li>
-        <li><Link to="/codeblock/4">Event loop</Link></li>
-      </ul>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Choose code block</h1>
+            <ul>
+                <li><Link to="/codeblock/1">Async case</Link></li>
+                <li><Link to="/codeblock/2">Callback hell</Link></li>
+                <li><Link to="/codeblock/3">Promise example</Link></li>
+                <li><Link to="/codeblock/4">Event loop</Link></li>
+            </ul>
+        </div>
+    );
 };
 
 export default Lobby;
